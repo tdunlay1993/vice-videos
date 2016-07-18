56d2eef5571e86b12eafa94d4290b3dc94d8a9b2
Thomas Dunlay <tomdunlay1@gmail.com>
2016-07-18 09:26:32 -0600

    Comitted phase 1 of database design

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+
+		<!--adding custom css-->
+		<link href="style.css" rel="stylesheet" type="text/css"/>
+		
+		<title>About me</title>
+	</head>
+	<body>
+		<header>
+			<h1>Phase 1 - "Vice Featured Shows" </h1>
+		</header>
+
+		<main>
+
+			<br><h2> Persona </h2>
+
+			<ul>
+				<li>Age = 14-60</li>
+				<li>Profession = Broad Range</li>
+				<li>Technology = Wide Variety, Mostly Mac</li>
+				<li> Political Viewpoints = Mostly Liberal </li>
+				<li> Interests = Most Likely Heavy Interest in art and Music,
+				Changing due due growing news section though</li>
+			</ul>
+
+			<br><h2> Use Case </h2>
+				<p> User gets home from work. User wants to find some entertainment on the internet,
+					while also learning about the current affairs of the world.</p>
+
+			<br><h2> Interaction Flow </h2>
+			<ol>
+				<li>User scrolls through video sections, user’s attention gets directed towards a certain show </li>
+				<li>Since the person is has just started skateboarding </li>
+				<li>He clicks on epicly latered, a show that creates a profile for skateboarders. </li>
+				<li>The list of Epicly later’d episodes comes up. </li>
+				<li>After looking through skaters the user finds his/her favorite skater, Ed templeton. </li>
+				<li>User Clicks Link </li>
+				<li>User Presses Play, Proceeds to watch Ed Templeton Documentary</li>
+			</ol>
+			<br><h2> Conceptual Model</h2>
+				<br> <img src="images/cmodel.jpg" alt="cmodel" width="700" height="700"/>
+		</main>
+
+
+
+		<p></p>
+	</body>
+</html>
